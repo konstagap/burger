70803f51b47e87243aed931fd3a0543def44b3f2
konstagap <konstagapov123@gmail.com>
2019-10-08 20:08:23 -0700

    logic is build, needs styling

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('sweet onion burger');
+INSERT INTO burgers (burger_name) VALUES ('beetroot burger');
+INSERT INTO burgers (burger_name) VALUES ('Bobbi burger');
+INSERT INTO burgers (burger_name,devoured ) VALUES ('Lili burger',1);
