60f1cd2643a92b9121859dc014f12215ab594462
konstagap <konstagapov123@gmail.com>
2019-10-09 20:27:00 -0700

    added jawsbd url connection

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,16 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
-
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+  });
+}
 // Make connection.
 connection.connect(function(err) {
   if (err) {
